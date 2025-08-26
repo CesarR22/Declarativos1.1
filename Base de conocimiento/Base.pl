831f47b09b121e022ffdd5251dc220351e10a576
Cesar Roque <00015319@uca.edu.sv>
2025-08-26 16:43:41 -0600

    Idea

---
+++ Base de conocimiento/Base.pl
@@ -0,0 +1,48 @@
+%Parte#1
+
+matrimonio(esteban,mariela).
+matrimonio(alonso,beatriz).
+matrimonio(clara,andres).
+matrimonio(rodolfo,teresa).
+matrimonio(catalina,andres).
+
+amistad(ismael,hector).
+
+hijo_de(clara,esteban).
+hijo_de(rodolfo,esteban).
+hijo_de(ismael,esteban).
+hijo_de(clara,mariela).
+hijo_de(rodolfo,mariela).
+hijo_de(ismael,mariela).
+
+hijo_de(felipe,alonso).
+hijo_de(catalina,alonso).
+hijo_de(emilia,alonso).
+hijo_de(felipe,beatriz).
+hijo_de(catalina,beatriz).
+hijo_de(emilia,beatriz).
+
+padre_de(andres,sofia).
+padre_de(clara,sofia).
+padre_de(rodolfo,lucas).
+padre_de(teresa,lucas).
+padre_de(rodolfo,martina).
+padre_de(teresa,martina).
+
+%Parte#5
+amenaza(saqueadores_montañosos).
+
+propone(don_esteban, unir_aldeas_contra_saqueadores).
+rechaza(don_alonso, unir_aldeas).
+
+sueño(sofia, proyectos_culturales).
+sueño(sofia, estudiar_medicina_con_catalina).
+sueño(lucas, continuar_herreria).
+sueño(martina, aprender_ceramica).
+
+aprendiz(lucas, hector).
+aspira(sofia, medicina_con_catalina).
+aspira(martina, ceramica_con_emilia).
+
+proyecto_compartido(cultura).
+proyecto_compartido(educacion).
