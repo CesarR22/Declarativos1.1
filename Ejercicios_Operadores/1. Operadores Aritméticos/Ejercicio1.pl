3b9bfd8187e471b1e92f6625fd5fb891fe5c4b47
Cesar Roque <00015319@uca.edu.sv>
2025-09-04 16:28:42 -0600

    Operaciones

--- Ejercicios_Operadores/1. Operadores Aritméticos/Ejercicio1.pl
+++
@@ -1,7 +0,0 @@
-rectangulo(Area, Perimetro) :-
-    write('Introduce la base: '),
-    read(Base),
-    write('Introduce la altura: '),
-    read(Altura),
-    Area is Base * Altura,
-    Perimetro is 2 * (Base + Altura).
